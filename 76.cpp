8a27de3d03f1ed91b7d6e9d46e0efe0f30a20250
xKarfi <60159438+xKarfi@users.noreply.github.com>
2022-03-08 18:30:27 +0100

    Update 76.cpp

--- 76.cpp
+++ 76.cpp
@@ -2,107 +2,98 @@
 #include <fstream>
 #include <vector>
 using namespace std;
-class code
+class task
 {
     public:
-        ifstream f1;
-        ifstream f2;
-        ifstream f3;
-        ofstream fOut;
+        ifstream file1;
+        ifstream file2;
+        ifstream file3;
 
-        code();
-        ~code();
-        
-        string script(string word, vector <int> code);
-        string unscript(string word, vector <int> code);
+        task();
+        ~task();
+        string sc(string word, vector <int> task);
+        string unsc(string word, vector <int> task);
 };
-
-code::code() 
+task::task() 
 {
-    f1.open("szyfr1.txt");
-    f2.open("szyfr2.txt");
-    f3.open("szyfr3.txt");
-    fOut.open("wyniki_szyfru.txt");
+    file1.open("szyfr1.txt");
+    file2.open("szyfr2.txt");
+    file3.open("szyfr3.txt");
 }
 
-code::~code() 
+task::~task() 
 {
-    f1.close();
-    f2.close();
-    f3.close();
-    fOut.close();
+    file1.close();
+    file2.close();
+    file3.close();
 }
-
-string code::unscript(string word, vector <int> code) 
+string task::unsc(string word, vector <int> task) 
 {
     int length = word.length();
     for (int i = length; i >= 0; i--) 
 	{
-        int buf = word[i];
-        
-        word[i] = word[code[i % code.size()] - 1];
-        word[code[i % code.size()] - 1] = buf;
+        int bb = word[i];
+        word[task[i % task.size()] - 1] = bb;
+        word[i] = word[task[i % task.size()] - 1];
     }
     return word;
 }
-string code::script(string word, vector <int> code) 
+string task::sc(string word, vector <int> task) 
 {
     int length = word.length();
     for (int i = 0; i < length ; i++) 
 	{
-        int buf = word[i];
-        
-        word[i] = word[code[i % code.size()]-1];
-        word[code[i % code.size()]-1] = buf;
+        int bb = word[i];
+        word[task[i % task.size()]-1] = bb;
+        word[i] = word[task[i % task.size()]-1];
     }
     return word;
 }
 
 int main()
 {
-    code c1;
+    task c1;
     cout << "76.1:" << endl;
     vector <string> s1;
     for (int i = 0; i < 6; i++) 
 	{
-        string prov;
-        c1.f1 >> prov;
-        s1.push_back(prov);
+        string pro;
+        c1.file1 >> pro;
+        s1.push_back(pro);
     }
-    vector <int> code1;
-    while (!c1.f1.eof()) 
+    vector <int> task1;
+    while (!c1.file1.eof()) 
 	{
-        int prov;
-        c1.f1 >> prov;
-        code1.push_back(prov);
+        int pro;
+        c1.file1 >> pro;
+        task1.push_back(pro);
     }
     for (int i = 0; i < 6; i++) 
 	{
-        cout << c1.script(s1[i], code1)<<endl;
+        cout << c1.sc(s1[i], task1)<<endl;
     }
     
-
     string s2;
-    vector <int> code2;
-    c1.f2 >> s2;
-    while (!c1.f2.eof()) 
+    vector <int> task2;
+    c1.file2 >> s2;
+    while (!c1.file2.eof()) 
 	{
-        int prov;
-        c1.f2 >> prov;
-        code2.push_back(prov);
+        int pro;
+        c1.file2 >> pro;
+        task2.push_back(pro);
     }
     cout << endl << "76.2:" << endl;
-    cout << c1.script(s2, code2) << endl;
+    cout << c1.sc(s2, task2) << endl;
     cout << endl << "76.3:" << endl;
     string s3;
-    c1.f3 >> s3;
-    vector <int> code3;
-    code3.push_back(6);
-    code3.push_back(2);
-    code3.push_back(4);
-    code3.push_back(1);
-    code3.push_back(5);
-    code3.push_back(3);
-    cout << c1.unscript(s3, code3);
+    c1.file3 >> s3;
+    vector <int> task3;
+    task3.push_back(6);
+    task3.push_back(2);
+    task3.push_back(4);
+    task3.push_back(1);
+    task3.push_back(5);
+    task3.push_back(3);
+    cout << c1.unsc(s3, task3);
 }
 
